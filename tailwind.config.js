e96c5d0f5eea93da4362974b1af9dec99912a4f7
Pankaj Ganjale <pankajganjale2805@gmail.com>
2023-06-13 21:31:05 +0530

    Updated tailwind config for responsiveness:

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,20 @@ module.exports = {
       //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       // },
     },
+    screens: {
+      '1sm': '320px',
+      // => @media (min-width: 640px) { ... }
+      'sm': '480px',
+      'md': '640px',
+      // => @media (min-width: 640px) { ... }
+      'lg': '768px',
+      // => @media (min-width: 768px) { ... }
+      'xl': '1024px',
+      // => @media (min-width: 1024px) { ... }
+      '2xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      '3xl': '1536px',
+    }
   },
   plugins: [],
 }
